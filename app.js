6465388c4fddfb634508a562b7db1e30b1b3f4eb
Razaurrehman <razaurrehman1991@hotmail.com>
2018-07-20 04:07:55 +0500

    make it better

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname+'dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
